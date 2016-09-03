3d994372057c87e56e3796fcefd8f9df7f1c8be7
Tiantian <azuo1228@outlook.com>
2016-09-03 23:25:46 +0800

    Fix jsHint issue

--- config/lib/express.js
+++ config/lib/express.js
@@ -91,8 +91,8 @@ module.exports.initMiddleware = function (app) {
 
   // Enable the CORS -- Cross-Origin Resource Sharing
   app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type');
     next();
   });
 };
